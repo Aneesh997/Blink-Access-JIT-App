62a08239ee671d319a4c5e715f61605291366cc4
Aneesh Adithya S R <155720294+Aneesh997@users.noreply.github.com>
2026-06-16 04:28:42 +0530

    Update aws-config.js

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,9 +1,9 @@
 export const awsConfig = {
   Auth: {
     region: 'ap-south-1',
-    userPoolId: 'ap-south-1_q0Da2VSPE',
-    userPoolWebClientId: 'j73qvgbu2jqpo16tp84f19cu1'
+    userPoolId: '****',
+    userPoolWebClientId: '****'
   }
 };
 
-export const API_BASE = 'https://suymyue1u3.execute-api.ap-south-1.amazonaws.com/prod';
+export const API_BASE = 'https://suymyue1u3.execute-api.ap-south-1.amazonaws.com/****';
